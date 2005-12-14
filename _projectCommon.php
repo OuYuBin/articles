15b2d28174006793eb782c18ab1b2bfe30b2c55d
wbeaton <wbeaton>
2005-12-14 21:23:07 +0000

    Changed levels of left nav items.

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,8 +8,8 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	$Nav->addNavSeparator("Articles", "index.php");
-	$Nav->addCustomNav("RCP Articles", "index.php?filter=rcp", "_self", 2);
-	$Nav->addCustomNav("Contributing", "contributing.php", "_self", 2);
-	$Nav->addCustomNav("Contact the editor", "mailto:articles_editor@eclipse.org", "_self", 2);
+	$Nav->addCustomNav("RCP Articles", "index.php?filter=rcp", "_self", 1);
+	$Nav->addCustomNav("Contributing", "contributing.php", "_self", 1);
+	$Nav->addCustomNav("Contact the editor", "mailto:articles_editor@eclipse.org", "_self", 1);
 
 ?>
