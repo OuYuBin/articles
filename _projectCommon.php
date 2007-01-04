fb672d4f67e747b6ffa3546424ffb609ae5e10c6
wbeaton <wbeaton>
2007-01-04 20:03:21 +0000

    [162508] Broken link on left hand side of articles page https://bugs.eclipse.org/bugs/show_bug.cgi?id=162508

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,7 +8,6 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	$Nav->addNavSeparator("Articles", "index.php");
-	$Nav->addCustomNav("RCP Articles", "index.php?filter=rcp", "_self", 1);
 	$Nav->addCustomNav("Contributing", "contributing.php", "_self", 1);
 	$Nav->addCustomNav("Suggest a new article", "https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Community&component=Articles", "_self", 1);
 
