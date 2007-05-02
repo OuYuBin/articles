56d50d21f1b511e1d516580f52706cbe40cac277
wbeaton <wbeaton>
2007-05-02 01:36:06 +0000

    Directory clean up. Removed a bunch of unnecessary files.

--- index.php
+++ index.php
@@ -102,9 +102,6 @@
  		$articles
 	</div>
 	<div id="rightcolumn">
-		<div class="sideitem" style="border:NONE;text-align:center">
-			<a href="http://www.eclipsecon.org/?tag=eclipse-articles"><img src="http://www.eclipsecon.org/2007/image125x125.gif"/></a>
-		</div>
 		<div class="sideitem">
 			<h6>Categories</h6>
 			$category_cloud
