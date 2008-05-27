e2cc456e3a5efd871dbf56f3aee48deb5a0ef528
wbeaton <wbeaton>
2008-05-27 20:06:33 +0000

    NEW - bug 234235: Eclipse Corner should use Google Search for searching articles https://bugs.eclipse.org/bugs/show_bug.cgi?id=234235

--- index.php
+++ index.php
@@ -91,13 +91,13 @@
 			<a href="/community/">Eclipse Community page</a>. You can find these articles
 			and more on the <a href="/resources">Eclipse Resources</a> page.
 		</p>
+		<form action="http://www.google.com/cse" id="searchbox_017941334893793413703:sqfrdtd112s">
+	 	<input name="cx" value="017941334893793413703:sqfrdtd112s" type="hidden">
+	 	<input type="hidden" name="hq" value="inurl:eclipse.org/articles">
+  		<input style="background: rgb(255, 255, 255) url(http://www.google.com/coop/intl/en/images/google_custom_search_watermark.gif) no-repeat scroll left center; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial;" name="q" size="25" type="text">
+  		<input name="sa" value="Search" type="submit">
+		</form>
 		
-		<form method="get" action="/search/search.cgi">
-        	<input type="hidden" name="ul" value="articles" />
-			<input type="hidden" name="wf" value="574a74"; />
-			Search Articles: <input type="text" name="q" value="" />
-			<input type="submit" value="Search!" />
- 		</form>
  		<hr/>
  		$articles
 	</div>
